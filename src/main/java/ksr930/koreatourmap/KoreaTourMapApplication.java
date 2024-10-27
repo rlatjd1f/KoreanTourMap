9774872eeb4d8b0da1ac362e16267ff1beffa3da
rlatjd1f <dl290382@gmail.com>
2024-10-27 13:58:12 +0900

    init

---
+++ src/main/java/ksr930/koreatourmap/KoreaTourMapApplication.java
@@ -0,0 +1,13 @@
+package ksr930.koreatourmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaTourMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaTourMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksr930/koreatourmap/KoreaTourMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ksr930.koreatourmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaTourMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
