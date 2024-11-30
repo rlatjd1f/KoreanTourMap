5a99defe33d0a945fbe2ae8f3af822a501c579fb
rlatjd1f <dl290382@gmail.com>
2024-11-30 23:15:16 +0900

    css 분리, 패널 추가

--- src/main/java/ksr930/koreatourmap/controller/MapController.java
+++ src/main/java/ksr930/koreatourmap/controller/MapController.java
@@ -3,7 +3,7 @@ package ksr930.koreatourmap.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
